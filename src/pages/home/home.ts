baf9353c0782986facd781687d80a354ab47909b
Divaughnn99 <46171705+Divaughnn99@users.noreply.github.com>
2019-03-02 14:02:49 -0800

    change date added

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+// import { DatePicker } from '@ionic-native/date-picker/ngx';
+// constructor(private datePicker: DatePicker) { }
+
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
@@ -8,6 +12,7 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
+
 
   }
   ionViewDidLoad() {
@@ -23,6 +28,11 @@ function main(){
   // get date/time school Ends
   // 5/16/19
   var t2 = new Date(2019, 4, 16, 0, 0, 0, 0);
+  console.log(document.getElementById("oink").value)
+  var dateInput = document.getElementById("oink").value;
+  if (dateInput){
+    t2 = new Date(dateInput);
+  }
   // find differance
   var dif = t1.getTime() - t2.getTime();
   var Seconds_from_T1_to_T2 = dif / 1000;
@@ -47,59 +57,85 @@ function main(){
   theDiv.innerHTML = daysBetweenDates + " Days";
 
 
-  // update number
+  // update number 
 
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function date() {
-  const monthNames = ["January", "February", "March", "April", "May", "June",
-    "July", "August", "September", "October", "November", "December"
-  ];
-    var qntYears = 4;
-    var selectYear = $("#year");
-    var selectMonth = $("#month");
-    var selectDay = $("#day");
-    var currentYear = new Date().getFullYear();
-
-    for (var y = 0; y < qntYears; y++){
-      let date = new Date(currentYear);
-      var yearElem = document.createElement("option");
-      yearElem.value = currentYear
-      yearElem.textContent = currentYear;
-      selectYear.append(yearElem);
-      currentYear--;
-    }
-
-    for (var m = 0; m < 12; m++){
-        let monthNum = new Date(2018, m).getMonth()
-        let month = monthNames[monthNum];
-        var monthElem = document.createElement("option");
-        monthElem.value = monthNum;
-        monthElem.textContent = month;
-        selectMonth.append(monthElem);
-      }
-      var d = new Date();
-      var month = d.getMonth();
-      var year = d.getFullYear();
-      var day = d.getDate();
-      selectYear.val(year);
-      selectYear.on("change", AdjustDays);
-      selectMonth.val(month);
-      selectMonth.on("change", AdjustDays);
-      AdjustDays();
-      selectDay.val(day)
-      function AdjustDays(){
-        var year = selectYear.val();
-        var month = parseInt(selectMonth.val()) + 1;
-        selectDay.empty();
-        //get the last day, so the number of days in that month
-        var days = new Date(year, month, 0).getDate();
-        //lets create the days of that month
-        for (var d = 1; d <= days; d++){
-          var dayElem = document.createElement("option");
-          dayElem.value = d;
-          dayElem.textContent = d;
-          selectDay.append(dayElem);
-        }
-      }
+  // const monthNames = ["January", "February", "March", "April", "May", "June",
+  //   "July", "August", "September", "October", "November", "December"
+  // ];
+  //   var qntYears = 4;
+  //   var selectYear = getElementById("year");
+  //   var selectMonth = getElementById("month");
+  //   var selectDay = getElementById("day");
+  //   var currentYear = new Date().getFullYear();
+  //
+  //   for (var y = 0; y < qntYears; y++){
+  //     let date = new Date(currentYear);
+  //     var yearElem = document.createElement("option");
+  //     yearElem.value = currentYear
+  //     yearElem.textContent = currentYear;
+  //     selectYear.append(yearElem);
+  //     currentYear--;
+  //   }
+  //
+  //   for (var m = 0; m < 12; m++){
+  //       let monthNum = new Date(2018, m).getMonth()
+  //       let month = monthNames[monthNum];
+  //       var monthElem = document.createElement("option");
+  //       monthElem.value = monthNum;
+  //       monthElem.textContent = month;
+  //       selectMonth.append(monthElem);
+  //     }
+  //     var d = new Date();
+  //     var month = d.getMonth();
+  //     var year = d.getFullYear();
+  //     var day = d.getDate();
+  //     selectYear.val(year);
+  //     selectYear.on("change", AdjustDays);
+  //     selectMonth.val(month);
+  //     selectMonth.on("change", AdjustDays);
+  //     AdjustDays();
+  //     selectDay.val(day)
+  //     function AdjustDays(){
+  //       var year = selectYear.val();
+  //       var month = parseInt(selectMonth.val()) + 1;
+  //       selectDay.empty();
+  //       //get the last day, so the number of days in that month
+  //       var days = new Date(year, month, 0).getDate();
+  //       //lets create the days of that month
+  //       for (var d = 1; d <= days; d++){
+  //         var dayElem = document.createElement("option");
+  //         dayElem.value = d;
+  //         dayElem.textContent = d;
+  //         selectDay.append(dayElem);
+  //       }
+  //     }
 }
